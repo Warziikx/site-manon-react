bf57391bff0a39ef92f9a2c06f1fd944e885695d
Nicolas Durand <nicolas.durand@sotiaf.fr>
2023-10-25 10:15:46 +0200

    :bug: fix prerendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-const rootElement = document.getElementById("root");
-
-if (rootElement!.hasChildNodes()) {
-	ReactDOM.hydrate(<App />, rootElement);
-} else {
-	ReactDOM.render(<App />, rootElement);
-}
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,36 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { htmlPrerender } from "vite-plugin-html-prerender";
 import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	build: { outDir: './build' },
+	plugins: [
+		react(),
+		htmlPrerender({
+			/**
+			 * Required: Output directory of "vite build"
+			 */
+			staticDir: path.join(__dirname, "build"),
+
+			/**
+			 * Required: List of routes to pre-render.
+			 */
+			routes: ["/", "/contact"],
+
+			/**
+			 * Optional: To minify html. Uses https://github.com/kangax/html-minifier.
+			 */
+			minify: {
+				collapseBooleanAttributes: true,
+				collapseWhitespace: true,
+				decodeEntities: true,
+				keepClosingSlash: true,
+				sortAttributes: true,
+			},
+		}),
+	],
+	build: { outDir: "./build" },
 	resolve: {
 		alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
 	},

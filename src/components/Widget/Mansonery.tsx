f2d941f50516470888fbf7ee2839018c3e1d87ac
Nicolas Durand <nicolas.durand@sotiaf.fr>
2023-11-28 22:14:14 +0100

    :sparkles: Mansonery

--- src/components/Widget/Mansonery.tsx
+++ src/components/Widget/Mansonery.tsx
@@ -10,9 +10,9 @@ export const Mansonery: React.FC<MansoneryProps> = ({ data }) => {
   const mansoneryRef = useRef(null);
   const { scrollYProgress } = useScroll({ target: mansoneryRef, offset: ["start end", "end -20vh"] });
 
-  const y1 = useTransform(scrollYProgress, [0, 1], [100, -1200]);
-  const y2 = useTransform(scrollYProgress, [0, 1], [-300, -1300]);
-  const y3 = useTransform(scrollYProgress, [0, 1], [-100, -1600]);
+  const y1 = useTransform(scrollYProgress, [0, 1], [400, -1200]);
+  const y2 = useTransform(scrollYProgress, [0, 1], [300, -1300]);
+  const y3 = useTransform(scrollYProgress, [0, 1], [500, -1600]);
   const y4 = useTransform(scrollYProgress, [0, 1], [-300, -1400]);
   //   const y1 = useTransform(scrollYProgress, [0, 1], [-400, -1200]);
   //   const y2 = useTransform(scrollYProgress, [0, 1], [-300, -1300]);
@@ -21,16 +21,20 @@ export const Mansonery: React.FC<MansoneryProps> = ({ data }) => {
   const yArray = [y1, y2, y3, y4];
   return (
     <div
-      className="grid h-screen grid-cols-2 gap-4 overflow-hidden bg-link-water p-8 md:grid-cols-4 lg:px-32"
+      className={`grid h-screen grid-cols-1 sm:grid-cols-2 lg:grid-cols-${data.length} gap-4 overflow-hidden bg-link-water p-8 lg:px-32`}
       ref={mansoneryRef}
     >
       {data.map((mans: IMansonery, index: number) => (
-        <motion.div className="grid gap-4" style={{ y: yArray[index] }}>
+        <motion.div
+          key={mans.ID}
+          className={`grid gap-4 ${index === 1 && "max-sm:hidden"} ${index === 2 && "max-lg:hidden"}`}
+          style={{ y: yArray[index] }}
+        >
           {mans.images
             .sort((a: IMansoneryObj, b: IMansoneryObj) => a.ordre - b.ordre)
             .map((item: IMansoneryObj) => {
               return (
-                <div key={item.image}>
+                <div key={item.ID}>
                   <img className={`h-auto max-w-full rounded-lg`} src={"/" + item.image} alt="" />
                 </div>
               );

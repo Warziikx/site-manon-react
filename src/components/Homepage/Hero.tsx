3d5f83890c620edefcc6b7633506129ec9c0f629
Nicolas Durand <nicolas.durand@sotiaf.fr>
2023-12-14 23:26:04 +0100

    :bug: SEO and Image

--- src/components/Homepage/Hero.tsx
+++ src/components/Homepage/Hero.tsx
@@ -15,7 +15,7 @@ export const Hero: React.FC = () => {
   useEffect(() => {
     if (isInView) {
       controls.start({ scale: [0.2, 1], rotate: [90, 0] });
-      controls2.start({ scale: [0.2, 1], rotate: [-90, 0] });
+      controls2.start({ scale: [0.2, 1], rotate: [-90, -180] });
     }
   }, [controls, isInView]);
 
@@ -25,33 +25,37 @@ export const Hero: React.FC = () => {
         <motion.img
           ref={ref}
           src={fleur2}
+          alt="Illustation de fleur terracotta"
           className="absolute -top-16 left-4 w-6/12 md:-left-64"
           animate={controls}
           transition={{ duration: 1.2 }}
         />
-        <motion.img
-          src={fleur3}
-          animate={controls2}
-          transition={{ duration: 1.2 }}
-          className="absolute -left-12 bottom-0 md:bottom-4 w-64 rotate-6 md:left-0 md:w-96 xl:left-32"
-        />
         <div className="relative m-auto text-center">
-          <img src={logo} className="mx-auto max-w-3xl" />
-          <p className="relative mx-auto -mt-16 text-lg md:text-2xl text-primary">
+          <img src={logo} className="mx-auto w-full md:w-8/12" />
+          <p className="relative mx-auto text-lg md:text-2xl text-primary">
             De la capture de vos instants précieux,
             <br /> à la création minutieuse de vos projets graphique
           </p>
         </div>
         <motion.img
           src={fleur1}
-          animate={controls}
+          alt="Illustation de fleur rose"
+          animate={controls2}
           transition={{ duration: 1.2 }}
-          className="absolute -right-12 top-24 md:top-32 w-4/12 rotate-180 sm:-right-24 md:block"
+          className="absolute -right-12 w-4/12 rotate-180 sm:-right-24 md:block"
         />
       </div>
       <motion.img
-        src={fleur2}
+        src={fleur3}
+        alt="Illustation de fleur verte"
         animate={controls2}
+        transition={{ duration: 1.2 }}
+        className="absolute -left-12 bottom-8 md:-bottom-20 w-64 rotate-90 md:left-0 md:w-96 xl:left-32"
+      />
+      <motion.img
+        src={fleur2}
+        alt="Illustation de fleur bleu"
+        animate={controls}
         transition={{ duration: 1.2 }}
         className="absolute -bottom-2 right-8 z-10 w-32 rotate-45 md:-bottom-8 md:right-64"
       />

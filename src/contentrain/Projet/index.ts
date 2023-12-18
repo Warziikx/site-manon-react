439a8803c4a99c50b577ac6ea3d32e88c0556c19
Durand Nicolas <nicolas.durand@sotiaf.fr>
2023-12-18 19:45:48 +0100

    :bug: fix page portfolio

--- src/contentrain/Projet/index.ts
+++ src/contentrain/Projet/index.ts
@@ -1,38 +1,46 @@
 import _projet from "@/contentrain/Projet/Projet.json";
 
 export interface IProjetImg {
-  ID: string;
-  createdAt: number;
-  updatedAt?: number;
-  img: string;
-  order: number;
+	ID: string;
+	createdAt: number;
+	updatedAt?: number;
+	img: string;
+	order: number;
 }
 
 export interface IProjet {
-  ID: string;
-  createdAt: number;
-  updatedAt?: number;
-  libelle: string;
-  slug: string;
-  type: string;
-  brief: {
-    ID: string;
-    createdAt: number;
-    updatedAt?: number;
-    title: string;
-    content: string;
-  };
-  imageList: IProjetImg[];
-  detail: string;
+	ID: string;
+	createdAt: number;
+	updatedAt?: number;
+	libelle: string;
+	slug: string;
+	type: string;
+	brief: {
+		ID: string;
+		createdAt: number;
+		updatedAt?: number;
+		title: string;
+		content: string;
+	};
+	imageList: IProjetImg[];
+	detail: string;
+	other: {
+		ID: string;
+		createdAt: number;
+		updatedAt?: number;
+		title: string;
+		description: string;
+		imageList: IProjetImg[];
+	};
 }
 
 /* ---- On cast les contenu ---- */
 export const projetList = _projet as IProjet[];
 
 export const getBySlug = (slug: string) => {
-  return projetList.find((p: IProjet) => p.slug === slug);
+	return projetList.find((p: IProjet) => p.slug === slug);
 };
 
 export const getById = (ID: string) => {
-  return projetList.find((p: IProjet) => p.ID === ID);
+	return projetList.find((p: IProjet) => p.ID === ID);
 };
--- src/pages/Portfolio/PortfolioDetailPage.tsx
+++ src/pages/Portfolio/PortfolioDetailPage.tsx
@@ -4,32 +4,47 @@ import { IProjet, IProjetImg, getBySlug } from "@/contentrain/Projet";
 import { Navigate, useParams } from "@tanstack/react-router";
 
 export const PortfolioDetailPage: React.FC = () => {
-  const { portfolioSlug } = useParams({ from: portfolioDetailRoute.id });
-  const portfolio: IProjet | undefined = getBySlug(portfolioSlug);
-  if (portfolio === undefined) return <Navigate to="/portfolio" />;
+	const { portfolioSlug } = useParams({ from: portfolioDetailRoute.id });
+	const portfolio: IProjet | undefined = getBySlug(portfolioSlug);
+	if (portfolio === undefined) return <Navigate to="/portfolio" />;
 
-  return (
-    <div className="mt-0 md:mt-16 ">
-      <div className="py-16 space-y-16">
-        <div className="text-center">
-          <p className="text-sm text-ternary">{portfolio.type}</p>
-          <h1 className="font-title text-6xl font-semibold">{portfolio.libelle}</h1>
-        </div>
-        <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 px-8 md:px-16 lg:px-72">
-          {portfolio.imageList
-            .sort((a: IProjetImg, b: IProjetImg) => a.order - b.order)
-            .map((imgObj: IProjetImg) => (
-              <div className="flex justify-center">
-                <img className="rounded-lg max-h-auto md:max-h-96" src={`/${imgObj.img}`} />
-              </div>
-            ))}
-        </div>
-        <div className="px-8 md:px-16 lg:px-32 xl:px-64 text-center space-y-4">
-          <h4 className="text-3xl font-title">{portfolio.brief.title}</h4>
-          <p className="text-xl" dangerouslySetInnerHTML={{ __html: portfolio.brief.content }}></p>
-        </div>
-      </div>
-      <CallToAction />
-    </div>
-  );
+	return (
+		<div className="mt-0 md:mt-16 space-y-16">
+			<div className="space-y-16 py-16">
+				<div className="text-center">
+					<p className="text-sm text-ternary">{portfolio.type}</p>
+					<h1 className="font-title text-6xl font-semibold">{portfolio.libelle}</h1>
+				</div>
+				<div className="grid grid-cols-1 gap-4 px-8 sm:grid-cols-3 md:px-16 lg:px-72">
+					{portfolio.imageList
+						.sort((a: IProjetImg, b: IProjetImg) => a.order - b.order)
+						.map((imgObj: IProjetImg) => (
+							<div className="flex justify-center">
+								<img className="max-h-auto rounded-lg md:max-h-96" src={`/${imgObj.img}`} />
+							</div>
+						))}
+				</div>
+				<div className="space-y-4 px-8 text-center md:px-16 lg:px-32 xl:px-64">
+					<h4 className="font-title text-3xl">{portfolio.brief.title}</h4>
+					<p className="text-xl" dangerouslySetInnerHTML={{ __html: portfolio.brief.content }}></p>
+				</div>
+			</div>
+			<div>
+				<div className="space-y-4 px-8 text-center md:px-16 lg:px-32 xl:px-64">
+					<h4 className="font-title text-3xl">{portfolio.other.title}</h4>
+					<div className="grid grid-cols-1 gap-4 px-8 sm:grid-cols-3 md:px-16 lg:px-72">
+						{portfolio.other.imageList
+							.sort((a: IProjetImg, b: IProjetImg) => a.order - b.order)
+							.map((imgObj: IProjetImg) => (
+								<div className="flex justify-center">
+									<img className="max-h-auto rounded-lg md:max-h-96" src={`/${imgObj.img}`} />
+								</div>
+							))}
+					</div>
+					<p className="text-xl" dangerouslySetInnerHTML={{ __html: portfolio.other.description }}></p>
+				</div>
+			</div>
+			<CallToAction />
+		</div>
+	);
 };
